3fe7e06442c55699704f6fa96e35143e108454d1
GabrielSilper <gabrielsilper.main@gmail.com>
2023-02-25 15:12:09 -0400

    Adicionando cores para componentes desabilitados

--- src/style/theme.js
+++ src/style/theme.js
@@ -3,6 +3,8 @@ const theme = {
     background: '#F9F3FC',
     primary: '#6D3278',
     secondary: '#F9F3FC',
+    disabled1: '#999999',
+    disabled2: '#BCBCBC',
   },
   width: {
     small: '645px',
