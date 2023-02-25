d1ab524a8982849ab44c126d092c11b5655b4dbe
GabrielSilper <gabrielsilper.main@gmail.com>
2023-02-24 22:54:28 -0400

    ok

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,13 +1,9 @@
 const theme = {
   colors: {
-    background: '#d6e1f4',
-    primary: '#2158b4',
+    background: '#F9F3FC',
+    primary: '#7d2090',
     secondary: '#F9F3FC',
-  },
-  width: {
-    mobileAndTablet: '520px',
-    inputOnMobile: "200px"
-  },
+  }
 };
 
 export default theme;
